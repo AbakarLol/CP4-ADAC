50fc3e23743af28104aaaceba6776ff1afcd404e
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2024-09-06 19:16:45 +0100

    Edit Title, Style and Add response to each path

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://rawcdn.githack.com/kamikazechaser/administrative-divisions-db/master/api/"
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,11 +15,22 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/indexed/:id", (req, res) => {
+app.get("/indexed/:id", async (req, res) => {
     const id = req.params.id;
-    console.log(id);
-    res.render("index.ejs", {id : id});
-} );
+    try{
+        const response = await axios.get(API_URL + id + ".json");
+        const result = response.data;
+        console.log(result);
+        res.render("index.ejs", {data : result});
+       }catch(error){
+        console.log(`Failed to make a request: ${ error.message}` );
+        res.render("index.ejs", { data : error.data });
+
+        }
+    } );
+       
+
+
 
 app.listen(port, () => {
     console.log(`Server Running on port : ${port}`);
