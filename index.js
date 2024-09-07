5da891249941e59c1f19151fadaa6cb224116ade
AbakarLol <74581300+AbakarLol@users.noreply.github.com>
2024-09-07 15:36:53 +0100

    Comment and Explain

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 
-
+/* Inintialize base values */
 const app = express();
 const port = 3000;
 const API_URL = "https://rawcdn.githack.com/kamikazechaser/administrative-divisions-db/master/api/"
 
+
+/* Middleware for static files Like styles or images
+and Parser Middleware to parse pass data between front and back */
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+/* Render The first page on landing */
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
 
+/* The /indexed/:id use to get the area where the map was 
+clicked by passing the id of the country or island to the 
+back and use axio 
+ */
 app.get("/indexed/:id", async (req, res) => {
     const id = req.params.id;
     try{
@@ -30,7 +39,7 @@ app.get("/indexed/:id", async (req, res) => {
        
 
 
-
+/* Create a Server which is listening to the port 3000 */
 app.listen(port, () => {
     console.log(`Server Running on port : ${port}`);
 });
